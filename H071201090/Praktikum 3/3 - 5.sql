06674c6944ed9c12d0bfdd7cde533b35e8c705d6
Shofwans <shosis2406@gmail.com>
2022-10-06 10:43:52 +0800

    Praktikum 3

---
+++ H071201090/Praktikum 3/3 - 5.sql
@@ -0,0 +1,49 @@
+-- ALTER TABLE mahasiswa
+-- ADD no_telp VARCHAR (20) NOT NULL,
+-- ADD email VARCHAR(50) NOT NULL,
+-- ADD alamat VARCHAR(100),
+-- ADD keterangan VARCHAR(20) ;
+#jawaban no.3
+-- INSERT INTO mahasiswa (nama,nim,jk,no_telp,email,alamat,keterangan)
+-- VALUE 
+-- ("Muh. Shofwan Siswandi","H071201090","L","00220020618","shofwan@gmail.com","pettarani 10","Semester 5"),
+-- ("MICHIKO TJIANG","H071211013","P","00220020619","Michiko@gmail.com","Bekasi 10","Semester 3"),
+-- ("ARJUNA RIBAL","H071211032","L","00220020611","arjuna@gmail.com","jakarta","Semester 3"),
+-- ("DHIYAA UNNISA","H071211079","P","00220020612","dhiyaa@gmail.com","medan","Semester 3"),
+-- ("FIRMANSYAH","H071211070","L","00220020613","firman@gmail.com","cirebon","Semester 3"),
+-- ("EDGAR SILVANUS LIMBA","H071211026","L","00220020614","edgar@gmail.com","jakarta 2","Semester 3"),
+-- ("MUCHTAR ADAM AL-HAMID","H071211040'","L","00220020615","muchtar@gmail.com","bekasi 11","Semester 3"),
+-- ("ISRA NIRWANA NUR N. KALAU","H071211036","P","00220020616","isra@gmail.com","sudiang","Semester 3"),
+-- ("MUHAMMAD ERWIN ARIF","H071211059","L","00220020617","erwin@gmail.com","cirebon 10","Semester 3"),
+-- ("ANDI RAJA ZULFAQAR","H071211057","L","00220020511","andi@gmail.com","manado","Semester 3"),
+-- ("JONES AZARYA TOMASSOYAN","H071211050","L","00220020512","jones@gmail.com","palopo","Semester 3"),
+-- ("NUR AISYAH S","H071211054","P","00220020618","shofwan@gmail.com","pettarani 10","Semester 3");
+#SELECT* FROM mahasiswa
+
+
+#jawaban no.4
+-- INSERT INTO buku
+-- VALUE 
+-- ('naga kembar',1,'shofwan',2002),
+-- ('tv ajaib',2,'conan',1998),
+-- ('gunung tinggi',3,'samuel',2018),
+-- ('two piece',4,'eda',1999),
+-- ('uzumaki bayu',5,'saske',2000);
+-- INSERT INTO pinjam
+-- VALUE
+-- ('2022-10-25 23:59:59',17,1,1,1),
+-- ('2022-10-26 23:59:59',17,2,0,2),
+-- ('2022-10-27 23:59:59',17,3,1,3),
+-- ('2022-10-28 23:59:59',17,4,0,4),
+-- ('2022-10-29 23:59:59',17,5,1,5);
+#jawaban no.5
+-- INSERT INTO mahasiswa (nama,id_mahasiswa,nim,jk,no_telp,email,alamat,keterangan)
+-- VALUE 
+-- ('Naruto',18,'H071201091','P','081208567890',NULL,'Bekasi','Semester 12'),
+-- ('Goku',19,'H071201092','L','0891718116158','goku@gmail.com',NULL,'Semester 14');
+-- INSERT INTO mahasiswa (nama,id_mahasiswa,nim,jk,no_telp,email,keterangan)
+-- VALUE ('shadow',109,'H071201093','L','081208567890','shadow@gamil.com','Semester 12');
+SELECT* FROM mahasiswa
+
+
+
