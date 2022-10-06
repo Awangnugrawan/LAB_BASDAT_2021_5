90c31b1bc2d2ad935b972fc9bf0321baa0cc5451
Arjuna Ribal <arjuna.ribal@gmail.com>
2022-10-06 10:57:14 +0800

    Praktikum 3

---
+++ H071211032/Praktikum 3/Tugas_Praktikum_3(3-5).sql
@@ -0,0 +1,61 @@
+-- USE db_praktikum;
+
+-- ALTER TABLE mahasiswa
+-- MODIFY no_telp VARCHAR (20) NOT NULL,
+-- alamat VARCHAR (100),
+-- email VARCHAR (50) NOT NULL,
+-- keterangan VARCHAR (20);
+
+-- SELECT * FROM mahasiswa;
+
+
+
+#Nomor 3
+-- INSERT INTO mahasiswa (nama, nim, jk, no_telp, email, alamat, keterangan)
+-- VALUE ("Arjuna Ribal", "H071211032", "L", "082187482003", "arjuna.ribal@gmail.com", "Telkomas", "Semester 3"),
+-- ("Michiko Tjiang", "H071211013", "P", "082335368793", "michiko.tjiang14@gmail.com", "Batua Raya", "Semester 3"),
+-- ("Edgar Silvanus Limba", "H071211026", "L", "081230004236", "edgarsilvanus@gmail.com", "Antang", "Semester 3"),
+-- ("Jones Azarya", "H071211050", "L", "085256854637", "jonesazarya123@gmail.com", "Panakukkang", "Semester 3"),
+-- ("Dhiyaa Unnisa", "H071211079", "P", "0887435587359", "unnisadhiyaa25@gmail.com", "Maros", "Semester 3"),
+-- ("Muchtar Adam", "H071211040", "L", "0895398304141", "muchtaradam2002@gmail.com", "Gowa", "Semester 3"),
+-- ("Muhammad Erwin", "H071211059", "L", "081254498373", "muherwinarif31@gmail.com", "Pallangga", "Semester 3"),
+-- ("Firmansyah", "H071211070", "L", "085931042162", "frmnsyah33@gmail.com", "Pinrang", "Semester 3"),
+-- ("Isra Nirwana", "H071211036", "P", "082147121111", "isranirwana2409@gmail.com", "Veteran Utara", "Semester 3"),
+-- ("Andi Raja Zulfaqar", "H071211057", "L", "089503906266", "andirajazulfakar@gmail.com", "Tamalanrea", "Semester 3"),
+-- ("Nur Aisyah S", "H071211054", "P", "085256223076" ,"nuraisyah@gmail.com", "Peterani", "Semester 3"),
+-- ("Muh. Shofwan Siswandi", "H071201090", "L", "082290507134","muhshofwan@gmail.com", "Tamalanrea", "Semester 3");
+
+-- SELECT * FROM mahasiswa;
+
+
+
+#Nomor 4
+#a. Tambah Data tabel Buku
+-- INSERT INTO buku VALUE ("Harry Potter and the Cursed Child",2015,"Magic",1),
+-- ("World War 2",2018,"History",2),
+-- ("Fantastic Beasts and Where to Find them",2017,"Fantasy",3),
+-- ("Diary of the Wimpy Kid",2012,"Children",4),
+-- ("3 AM in the Park",2013,"Horror",5);
+
+#b. Tambah Data tabel Pinjam
+-- INSERT INTO pinjam VALUE ("2022-05-16 15:23:49", 7, 1, 1, 1),
+-- ("2022-05-16 15:23:49", 9, 4, 1, 2),
+-- ("2022-05-16 15:23:49", 11, 3, 0, 3),
+-- ("2022-02-19 12:30:15", 12, 2, 0, 4),
+-- ("2022-08-16 19:14:01", 14, 5, 1, 5);
+
+-- SELECT * FROM pinjam;
+
+
+
+#Nomor 5
+-- INSERT INTO mahasiswa (nama, nim, jk, no_telp, email, keterangan)
+-- VALUE ("Rio Rizky", "H071211098", "L", "082592562378", "riorizky@gmail.com", "", "Semester 3"),
+-- ("Geraldin Robert", "H071211099", "L", "0825925", "riorizky@gmail.com", "Daya", NULL),
+-- ("Roger Rabbit", "H073297622", "P", "08651525184", "rogerrabbit@gmail.com", "Semester 7"); 
+
+-- SELECT * FROM mahasiswa;
+-- 
+-- DESC mahasiswa;
+-- 
+
---
+++ H071211032/Praktikum 3/Tugas_Praktikum_3.sql
@@ -0,0 +1,22 @@
+-- USE classicmodels;
+
+#Nomor 1
+-- INSERT INTO employees VALUE (1025,"Ribal","Arjuna","x9553","arjunaribal@gmail.com","1",1002,"Senior VP");
+-- INSERT INTO employees VALUE (1759,"Chalvari","Andrean","x1235","andreanchalvari@gmail.com","1",1056,"Senior_VP_Marketing");
+-- INSERT INTO employees VALUE (1502,"Yogaswara","Jordan","x1095","jordanyogaswara@gmail.com","1",1102,"Sales Rep");
+
+
+
+#Nomor 2
+-- INSERT INTO offices VALUE ("8","Toraja","+6285623978455", "Jln Tongkonan Utara No 126", NULL, "ID", "Indonesia", "902657", "SA");
+
+-- UPDATE employees
+-- SET officeCode ="8"
+-- WHERE officeCode = "4" AND jobTitle = "Sales Rep"
+
+-- Untuk memeriksa:
+-- SELECT * FROM employees
+-- WHERE officeCode = "8";
+
+-- SELECT * FROM employees;;
+
