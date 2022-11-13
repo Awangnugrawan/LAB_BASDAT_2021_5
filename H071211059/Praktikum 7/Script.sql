02ce14b6db7734f746edaef7af61cbb86cb500b4
erwinarif31 <muherwinarif31@gmail.com>
2022-11-13 22:09:56 +0800

    nomor 3

--- H071211059/Praktikum 7/Script.sql
+++ H071211059/Praktikum 7/Script.sql
@@ -7,4 +7,9 @@ right join payments p2 using (customerNumber);
 select e.employeeNumber, e.lastName, e.firstName, o.city, e.officeCode 
 from employees e 
 join offices o using (officeCode)
-where e.officeCode = (select e2.officeCode from employees e2 group by e2.officeCode order by count(*) desc limit 1)
+where e.officeCode = (select e2.officeCode from employees e2 group by e2.officeCode order by count(*) desc limit 1);
+
+select p.productName , 
+	(select p2.productScale  from products p2 where p2.productCode = p.productCode)
+from products p 
+where p.productName like "%ford%";
