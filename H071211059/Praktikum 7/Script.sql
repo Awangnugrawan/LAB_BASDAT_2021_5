d1692fda7bd6068cb860be027778b86cfa62914c
erwinarif31 <muherwinarif31@gmail.com>
2022-11-13 22:15:36 +0800

    nomor 4

--- H071211059/Praktikum 7/Script.sql
+++ H071211059/Praktikum 7/Script.sql
@@ -8,8 +8,22 @@ select e.employeeNumber, e.lastName, e.firstName, o.city, e.officeCode
 from employees e 
 join offices o using (officeCode)
 where e.officeCode = (select e2.officeCode from employees e2 group by e2.officeCode order by count(*) desc limit 1);
+;
 
 select p.productName , 
 	(select p2.productScale  from products p2 where p2.productCode = p.productCode)
 from products p 
-where p.productName like "%ford%";
+where p.productName like "%ford%"
+;
+
+from customers c 
+join employees e on c.salesRepEmployeeNumber = e.employeeNumber
+join orders o using (customerNumber)
+join orderdetails o2 using (orderNumber)
+where o.orderNumber = 
+			(select orderNumber 
+			from orderdetails o 
+			group by orderNumber 
+			order by sum(o.quantityOrdered * o.priceEach) desc limit 1)
+;
+
