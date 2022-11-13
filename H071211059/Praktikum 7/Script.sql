1716d3ec4cd1f131e62dec317f79b0c07697774e
erwinarif31 <muherwinarif31@gmail.com>
2022-11-13 22:07:57 +0800

    nomor 2

--- H071211059/Praktikum 7/Script.sql
+++ H071211059/Praktikum 7/Script.sql
@@ -3,3 +3,8 @@ select distinct c.customerName,
 		(select min(amount) from payments p where p.customerNumber = c.customerNumber)
 from customers c
 right join payments p2 using (customerNumber);
+
+select e.employeeNumber, e.lastName, e.firstName, o.city, e.officeCode 
+from employees e 
+join offices o using (officeCode)
+where e.officeCode = (select e2.officeCode from employees e2 group by e2.officeCode order by count(*) desc limit 1)
