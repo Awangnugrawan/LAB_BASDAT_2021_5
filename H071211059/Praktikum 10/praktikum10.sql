025a1917c5b6abbf5128633d080974ea2615f97e
erwinarif31 <muherwinarif31@gmail.com>
2022-12-01 22:59:10 +0800

    Tugas Praktikum 10

--- H071211059/Praktikum 10/praktikum10.sql
+++ H071211059/Praktikum 10/praktikum10.sql
@@ -1,15 +1,14 @@
 use classicmodels;
 
-select  c.customerName,
-        case
-           when c.creditLimit - sum(o2.priceEach * o2.quantityOrdered) > 0 then 'You are safe'
-           when c.creditLimit - sum(o2.priceEach * o2.quantityOrdered) < 0 then 'You are in debt'
-           when c.creditLimit - sum(o2.priceEach * o2.quantityOrdered) = 0 then 'You are running out of credits'
-        end 'Are you safe?',
-        c.creditLimit - sum(o2.priceEach * o2.quantityOrdered) total
+select customerName,
+       case
+           when c.creditLimit - sum(o2.quantityOrdered * o2.priceEach) = 0 then 'You are running out of credits'
+            when c.creditLimit - sum(o2.quantityOrdered * o2.priceEach) < 0 then 'You are in debts'
+            when c.creditLimit - sum(o2.quantityOrdered * o2.priceEach) > 0 then 'You are safe'
+        end 'Are you safe',
+    c.creditLimit - sum(o2.quantityOrdered * o2.priceEach) total
 from customers c
 join orders o on c.customerNumber = o.customerNumber
-right join orderdetails o2 on o.orderNumber = o2.orderNumber
-group by o.customerNumber
-
-
+join orderdetails o2 on o.orderNumber = o2.orderNumber
+group by c.customerNumber
+order by c.customerName desc;
